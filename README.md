672eaa9e6f42e1a2e5702e7316855e1f7382a846
JeffyOLOLO <jeffyololo@gmail.com>
2021-08-19 23:26:22 +0300

    Сделал более подходящим для опенсорса

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # checkerOnline
 Простой дискорд бот, нужен только когда необходимо сделать рестарт при маленьком онлайне
+## Установка
+
+1. **Установите Python 3.6 или выше**
+
+2. **Клонируйте репозиторий**
+
+Используйте `git clone https://github.com/PerchunPak/checkerOnline.git`
+
+3. **Настроить виртуальную среду**
+
+Используйте `python3 -m venv .`
+
+4. **Закончив установку виртуальной среды, устанавливаем зависимости**
+
+Используйте `python3 -m pip install -U -r requirements.txt`
+
+---
+
+**Важная Заметка**: Вам нужно включить привилегию `SERVER MEMBERS` для работы бота. [Следуйте этим инструкциям.](https://discordpy.readthedocs.io/en/latest/intents.html#privileged-intents)
--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-from discord.ext import commands, tasks
-import discord
-import psutil
-import asyncio
+"""
+За основною часть кода спасибо https://github.com/PerchunPak/sunshinedsbot и конечно же https://github.com/NWordCounter/bot
+"""
+from discord.ext import commands
+from discord import Intents, Status, Activity, ActivityType, Client
+from psutil import Process
+from asyncio import sleep as asynSleep
 
-import datetime
-import os
+from datetime import datetime
+from os import getpid
 
-import mcstatus
+from mcstatus import MinecraftServer
 
 TOKEN = ''
 
-bot_intents = discord.Intents.default()
+bot_intents = Intents.default()
 bot_intents.members = True
 
 bot = commands.Bot(
@@ -18,59 +21,59 @@ bot = commands.Bot(
     description="пингер онлайна",
     case_insensitive=True,
     help_command=None,
-    status=discord.Status.invisible,
+    status=Status.invisible,
     intents=bot_intents,
     fetch_offline_members=True
 )
 
-bot.process = psutil.Process(os.getpid())
+bot.process = Process(getpid())
 bot.ready_for_commands = False
 
 
 @bot.event
 async def on_connect():
-    print("\nУстановлено соеденение с дискордом")
+    print('\nУстановлено соеденение с дискордом')
 
 
 @bot.event
 async def on_ready():
 
-    print("\nЗашел как:")
-    print(bot.user)
-    print(bot.user.id)
-    print("-----------------")
-    print(datetime.datetime.now().strftime("%m/%d/%Y %X"))
-    print("-----------------")
-    print("Шардов: " + str(bot.shard_count))
-    print("Серверов: " + str(len(bot.guilds)))
-    print("Пользователей: " + str(len(bot.users)))
-    print("-----------------\n")
+    print('\nЗашел как:\n'
+         f'{bot.user}\n'
+         f'{bot.user.id}\n'
+          '-----------------\n'
+         f'{datetime.now().strftime("%m/%d/%Y %X")}\n'
+          '-----------------\n'
+         f'Шардов: {str(bot.shard_count)}\n'
+         f'Серверов: {str(len(bot.guilds))}\n'
+         f'Пользователей: {str(len(bot.users))}\n'
+          '-----------------\n')
 
     bot.ready_for_commands = True
-    bot.started_at = datetime.datetime.utcnow()
+    bot.started_at = datetime.utcnow()
     bot.app_info = await bot.application_info()
 
-    await bot.change_presence(status=discord.Status.online, activity=discord.Activity(
-        name='скоро пингану онлайн', type=discord.ActivityType.playing))
+    await bot.change_presence(status=Status.online, activity=Activity(
+        name='скоро пингану онлайн', type=ActivityType.playing))
 
-    while True:
-        ping = mcstatus.MinecraftServer.lookup('localhost:25566')
+    while True: # собственно код программы
+        ping = MinecraftServer.lookup('localhost:25566') # айпи сервера1, тут это main сервер
         main = ping.status()
-        ping = mcstatus.MinecraftServer.lookup('localhost:25567')
+        ping = MinecraftServer.lookup('localhost:25567') # айпи сервера2, тут это rpg сервер
         rpg = ping.status()
         online = str(main.players.online + rpg.players.online)
         print('Онлайн: ' + online)
         
-        await bot.change_presence(status=discord.Status.online, activity=discord.Activity(
-            name=f'онлайн {online}', type=discord.ActivityType.playing))
+        await bot.change_presence(status=Status.online, activity=Activity(
+            name=f'онлайн {online}', type=ActivityType.playing))
 
-        if int(online) <= 5:
-            owner = bot.get_user(379353300887273472)
-            await owner.send(f'**Онлайн {online} так что поднимай свою жопу и иди готовится к переносу**')
-        elif int(online) <= 3:
-            owner = bot.get_user(379353300887273472)
-            await owner.send(f'**Онлайн {online} так что поднимай свою жопу и иди переносить пландб**')
-        await asyncio.sleep(300)
+        if int(online) <= 3:
+            owner = bot.get_user(bot.app_info.owner)
+            await owner.send(f'**Онлайн {online} так что готовься**')
+        elif int(online) <= 5:
+            owner = bot.get_user(bot.app_info.owner)
+            await owner.send(f'**Онлайн {online} так что делай что хотел**')
+        await asynSleep(300) # time.sleep не подходит тк останавливает все процессы
 
 
 
@@ -80,10 +83,10 @@ try:
     bot.loop.run_until_complete(bot.start(TOKEN))
 except KeyboardInterrupt:
     print("\nЗакрытие")
-    bot.loop.run_until_complete(bot.change_presence(status=discord.Status.invisible))
+    bot.loop.run_until_complete(bot.change_presence(status=Status.invisible))
     for e in bot.extensions.copy():
         bot.unload_extension(e)
     print("Выходим")
-    bot.loop.run_until_complete(discord.Client.close())
+    bot.loop.run_until_complete(Client.close())
 finally:
     print("Закрыто")
